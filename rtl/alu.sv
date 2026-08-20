150896de9fdf3b2b5df7f2b606475c873f6b2805
JackOwen Zarley <168610061+JZarley@users.noreply.github.com>
2026-08-19 18:33:25 -1000

    Implement RV32I ALU

---
+++ rtl/alu.sv
@@ -0,0 +1,46 @@
+module alu (
+    input logic [31:0] a,
+    input logic [31:0] b,
+    input riscv_pkg::alu_op_t alu_op,
+    output logic [31:0] result
+);
+
+    import riscv_pkg::*;
+
+    always_comb begin
+        result = '0;
+
+        unique case (alu_op)
+            ALU_ADD: begin
+                result = a + b;
+            end
+            ALU_SUB: begin
+                result = a - b;
+            end
+            ALU_AND: begin
+                result = a & b;
+            end
+            ALU_OR: begin
+                result = a | b;
+            end
+            ALU_XOR: begin
+                result = a ^ b;
+            end
+            ALU_SLL: begin
+                result = a << b[4:0];
+            end
+            ALU_SRL: begin
+                result = a >> b[4:0];
+            end
+            ALU_SRA: begin
+                result = $signed(a) >>> b[4:0];
+            end
+            ALU_SLT: begin
+                result = {{31{1'b0}}, $signed(a) < $signed(b)};
+            end
+            ALU_SLTU: begin
+                result = {{31{1'b0}}, a < b};
+            end
+        endcase
+    end
+endmodule
--- rtl/riscv_pkg.sv
+++ rtl/riscv_pkg.sv
@@ -10,4 +10,17 @@ package riscv_pkg;
         IMM_J
     } imm_sel_t;
 
+    typedef enum logic [3:0] {
+        ALU_ADD,
+        ALU_SUB,
+        ALU_AND,
+        ALU_OR,
+        ALU_XOR,
+        ALU_SLL,
+        ALU_SRL,
+        ALU_SRA,
+        ALU_SLT,
+        ALU_SLTU
+    } alu_op_t;
+
 endpackage
